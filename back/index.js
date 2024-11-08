2d0ad78ad6564d1b05aff0b6a4cb0fbc408d6b61
jesusparfer27 <jesusparfer@alumnos.cei.es>
2024-11-08 18:12:33 +0100

    commit

--- back/index.js
+++ back/index.js
@@ -6,7 +6,18 @@ import path from 'path'
 
 const app = express()
 
-app.use(cors());
+const allowedOrigins = ['https://junior-technical-test-front.vercel.app'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
